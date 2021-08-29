2c7a2e70673400a5dcd03d9d69e4e6d9750d9720
ENDI JEINNER ROMERO RODRIGUEZ <ejeinnerromero@ucompensar.edu.co>
2021-08-28 19:17:27 -0500

    Course Ionic - Bases Angular

---
+++ 02_bases-angular/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PagesComponent } from './pages/pages.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent, AboutComponent, PagesComponent, ContactComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
